cc60c5db1d6948d9c58f47ad36d65de6ec55bc98
6135 <guibscosta@hotmail.com>
2020-12-26 18:50:14 +0000

    Update Player.java

--- src/app/Player.java
+++ src/app/Player.java
@@ -4,10 +4,7 @@ public class Player {
     private String p;
     private Character c;
     private Player opponent;
-    public Player(String player, char c){
-        p=player;
-        this.c = c;
-    }
+    public Player(String player){p=player;}
 
     public void setSymbol(char c){this.c = c;}
     public char getSymbol(){return c;}
